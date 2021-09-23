4d7aa7e5c067eadc68c5c497c17a523dec8e9dfb
janghona <jang1na2@gmail.com>
2021-09-24 02:51:30 +0900

    [2292번] 벌집 문제
    * 육각형 벌집에서 입력된 숫자의 최소 거리를 찾는 문제
    * layer 1 = 1 / layer 2 = 6 / layer 3 = 6*2 / layer 4 = 6*3 -> 수열문제
    * layer 2의 최소값인 sum = 2 부터 시작해서 input값과 비교

--- BaekjoonCodeTest/BaekjoonCodeTest/BaekjoonCodeTest.cpp
+++ BaekjoonCodeTest/BaekjoonCodeTest/BaekjoonCodeTest.cpp
@@ -3,13 +3,17 @@ using namespace std;
 
 int main()
 {
-	int A;
-	int B;
+	int input = 0;
+	int layer = 0;
+	int sum = 2;
 
-	cin >> A >> B;
-	cout << A + B;
-
-	return 0;
+	cin >> input;
+	if (input == 1) layer = 1;
+	while (input >= sum) {
+		sum += 6 * layer;
+		layer++;
+	}
+	cout << layer;
 }
 
 
