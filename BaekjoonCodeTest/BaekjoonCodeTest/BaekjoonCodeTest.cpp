f1034ba9260f19394414acc682c196c1918a4ccf
janghona <jang1na2@gmail.com>
2021-09-24 22:11:20 +0900

    [10828번] 스택문제 다시 commit(수정)
    * stack 클래스로 구현 후 main에서 사용
    * push, pop, size, empty, top 기능 구현
    * command 명령어는 string을 사용 다른방법으론 strcmp char 사용하는 방법도 있음
    * 출력 후 한줄 씩 띄어쓰라는 설명이 있었는데 간과하고 있다가 시간 많이 잡아먹음
    (나의 생각을 너무 쓰지말고 질문자의 요구사항대로 프로그래밍을 하는것도 중요)

--- BaekjoonCodeTest/BaekjoonCodeTest/BaekjoonCodeTest.cpp
+++ BaekjoonCodeTest/BaekjoonCodeTest/BaekjoonCodeTest.cpp
@@ -1,19 +1,70 @@
-﻿#include <iostream>
+﻿#include<iostream>
+#include<string>
 using namespace std;
 
+class Stack {
+public:
+	int intStack[10000];
+	int ntop;
+public:
+	Stack() : ntop(-1)
+	{}
+	~Stack() {}
+
+	void push(int value) {
+		ntop++;
+		intStack[ntop] = value;
+	}
+
+	int pop() {
+		if (isEmpty() == true) return -1;
+		ntop--;
+		return intStack[ntop + 1];
+	}
+
+	int size() {
+		return ntop + 1;
+	}
+
+	bool isEmpty() {
+		if (ntop == -1) return true;
+		else return false;
+	}
+
+	int top() {
+		if (isEmpty() == true) return -1;
+		else return intStack[ntop];
+	}
+
+};
 int main()
 {
-	int input = 0;
-	int layer = 0;
-	int sum = 2;
+	Stack s;
+	int input;
+	string cmdBuf;
 
 	cin >> input;
-	if (input == 1) layer = 1;
-	while (input >= sum) {
-		sum += 6 * layer;
-		layer++;
+	if (1 > input || input > 10000) return 0;
+	for (int i = 0; i < input; i++) {
+		cin >> cmdBuf;
+
+		if (cmdBuf == "push") {
+			int num;
+			cin >> num;
+			s.push(num);
+		}
+		else if (cmdBuf == "top") {
+			cout << s.top() << endl;
+		}
+		else if (cmdBuf == "size") {
+			cout << s.size() << endl;
+		}
+		else if (cmdBuf == "empty") {
+			cout << s.isEmpty() << endl;
+		}
+		else if (cmdBuf == "pop") {
+			cout << s.pop() << endl;
+		}
 	}
-	cout << layer;
+	return 0;
 }
-
-
