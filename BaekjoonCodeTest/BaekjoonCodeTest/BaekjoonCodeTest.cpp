715338cbe1bdab392969b78da44e058732e27f16
janghona <jang1na2@gmail.com>
2021-09-29 04:15:57 +0900

    [2164번] 카드2 문제(큐 문제)
    * 차례대로 놓여있는 카드에서 제일 위 카드 pop 그다음 카드 맨 밑으로 가는 로직
    * 간단하게 pop 2번 하고 다시 push 하는 형태

--- BaekjoonCodeTest/BaekjoonCodeTest/BaekjoonCodeTest.cpp
+++ BaekjoonCodeTest/BaekjoonCodeTest/BaekjoonCodeTest.cpp
@@ -1,46 +1,21 @@
 ﻿#include <iostream>
 #include <queue>
-#include <string>
 using namespace std;
 
 int main() {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	queue <int> q;
-	int N, num;
-	string input;
-
+	queue<int> q;
+	int N;
 	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> input;
-		if (input.compare("push") == 0) {
-			cin >> num;
-			q.push(num);
-		}
-		else if (input.compare("pop") == 0) {
-			if (q.empty())
-				cout << -1 << '\n';
-			else {
-				num = q.front();
-				cout << num << '\n';
-				q.pop();
-			}
-		}
-		else if (input.compare("size") == 0)
-			cout << q.size() << '\n';
-		else if (input.compare("empty") == 0)
-			cout << q.empty() << '\n';
-		else if (input.compare("front") == 0) {
-			if (q.empty())
-				cout << -1 << '\n';
-			else
-				cout << q.front() << '\n';
-		}
-		else if (input.compare("back") == 0) {
-			if (q.empty())
-				cout << -1 << '\n';
-			else
-				cout << q.back() << '\n';
-		}
+
+	for (int i = 1; i <= N; i++) { q.push(i); }
+	while (q.size() != 1) {
+		q.pop();
+		int temp = q.front();
+		q.pop();
+		q.push(temp);
 	}
+	int result = q.front();
+	cout << result;
 }
