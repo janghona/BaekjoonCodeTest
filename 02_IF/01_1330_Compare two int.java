4f7de9a4f283c79f44b1e3c0fe57142fc92ee105
janghona <jang1na2@gmail.com>
2022-09-25 01:46:19 +0900

    java code practice
    -hello world
    -compare two int
    -times table

---
+++ 02_IF/01_1330_Compare two int.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        int a = 0;
+        int b= 0;
+        
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        
+        if(a < b){
+            System.out.print("<");
+        }
+          if(a > b){
+            System.out.print(">");
+        }
+          if(a == b){
+            System.out.print("==");
+        }
+    }
+}
